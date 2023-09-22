e7e05828fbe0c6ab367fa0ac41b788a68ce0bf0d
nastya.panysh@gmail.com <nastya.panysh@gmail.com>
2023-09-22 08:29:49 +0300

    del

--- server/src/helper/buildResponse.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from 'express';
-import { iReview, iUser } from '../interfaces/interfaces';
-
-function buildResponse(res: Response, status: number, message: string | iReview[] | iUser[]) {
-  res.status(status);
-  res.send(message);
-}
-
-export { buildResponse };
